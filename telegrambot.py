1ffbea75abfdba7a121dcd1594db89a51c3e54c3
roopakpcpc <roopakpcpc@gmail.com>
2020-09-04 14:56:50 +0530

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -1,6 +1,6 @@
 import os
-x = "roopakpcpc"
-y = 'aio_pLLt997gnBplcoIeduG5PNHiPF8N'
+x = os.getenv('x')
+y = os.getenv('y')
 
 !pip install python-telegram-bot
 !pip install adafruit-io
@@ -22,7 +22,7 @@ def off(bot,update):
   value=Data(value=0)
   value_send=aio.create_data('bot',value)
 
-u=Updater('1381707135:AAEyzALCkUUtx7fyKVFKQ9oD-HrVq8rCpIE')
+u=Updater(os.getenv('z'))
 dp=u.dispatcher
 dp.add_handler(CommandHandler('on',on))
 dp.add_handler(CommandHandler('off',off))
